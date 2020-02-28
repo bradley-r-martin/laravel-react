128a3a6cc57d40975cd517ef3127da606951d628
Brad Martin <bradmartin@brads-macbook-2.lan>
2020-02-28 16:48:17 +1100

    Init

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('{slug}','\BRM\React\app\Controllers\Routing@index')->where('slug', '(?!api|uploads).*(?<!js|css|json|html|txt|xml|webmanifest|jpg|gif|png|svg)$([A-z\d-\/_.]+)?');
+Route::get('{slug}','\BRM\React\app\Controllers\Routing@index')->where('slug', '(?!api|uploads).*(?<!js|css|json|html|txt|xml|webmanifest|jpg|gif|png|svg)');
